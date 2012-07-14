6f16cefb285c88630afbe25c2201bae684fe1842
Rainer Jung <Rainer.Jung@gmail.com>
2012-07-14 14:53:19 +0200

    Added github::zipball
    Should find a better way, not to have code just duplicated.

--- manifests/init.pp
+++ manifests/init.pp
@@ -18,3 +18,14 @@ define github::tarball($user, $project, $branch = 'master', $file = $title) {
   }
 }
 
+define github::zipball($user, $project, $branch = 'master', $file = $title) {
+  include github
+
+  exec { "curl-$title":
+    command => "curl -L -o '$file' 'http://github.com/$user/$project/zipball/$branch'",
+    unless  => "test -s '$file'",
+    path    => '/usr/bin:/usr/sbin',
+    require => Class['github'],
+  }
+}
+
