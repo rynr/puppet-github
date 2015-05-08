65d3c38ae5748e4c441588ee12ae33e416c1149d
Rainer Jung <Rainer.Jung@sinnerschrader.com>
2015-05-08 17:03:02 +0200

    Updated accordingly to lint results

--- manifests/tarball.pp
+++ manifests/tarball.pp
@@ -6,9 +6,9 @@ define github::tarball(
   $file = $title,
 ) {
 
-  exec { "curl-github-tar-$title":
-    command => "curl -L -o '$file' 'http://github.com/$user/$project/tarball/$branch'",
-    unless  => "test -s '$file'",
+  exec { "curl-github-tar-${title}":
+    command => "curl -L -o '${file}' 'http://github.com/${user}/${project}/tarball/${branch}'",
+    unless  => "test -s '${file}'",
     path    => '/usr/bin:/usr/sbin',
     require => Class['github'],
   }
--- manifests/zipball.pp
+++ manifests/zipball.pp
@@ -6,9 +6,9 @@ define github::zipball(
   $file = $title,
 ) {
 
-  exec { "curl-github-zip-$title":
-    command => "curl -L -o '$file' 'http://github.com/$user/$project/zipball/$branch'",
-    unless  => "test -s '$file'",
+  exec { "curl-github-zip-${title}":
+    command => "curl -L -o '${file}' 'http://github.com/${user}/${project}/zipball/${branch}'",
+    unless  => "test -s '${file}'",
     path    => '/usr/bin:/usr/sbin',
     require => Class['github'],
   }
